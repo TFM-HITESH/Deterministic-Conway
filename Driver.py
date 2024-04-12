faaa106e4f96c7a9fb6378ba0422903e0b51af38
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-04-12 20:56:01 +0530

    Deterministic Complete Commit

--- Driver.py
+++ Driver.py
@@ -1,14 +1,18 @@
 from Cell import Cell
 import random
+import time
+
+X_DIM = 10
+Y_DIM = 10
 
 def main():
     print("Start Position : ")
-    cellGrid = generateGrid(xDim=10, yDim=10, choice=1)
+    cellGrid = generateGrid(xDim=X_DIM, yDim=Y_DIM, choice=1)
     printGrid(cellGrid)
 
     startGrid=[[0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
                [0,0,0,0,0,1,0,0,0,0],
+               [0,0,0,0,1,1,0,0,0,0],
                [0,0,0,0,0,0,0,0,0,0],
                [0,0,0,0,0,0,1,0,0,0],
                [0,0,0,0,0,0,0,0,0,0],
@@ -17,17 +21,34 @@ def main():
                [0,0,0,0,0,0,0,0,0,0],
                [0,0,0,0,0,0,0,0,0,0]]
     
+    startGrid2=[[0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,1,0,0,0,0,0,0],
+               [0,0,0,0,1,0,0,0,0,0],
+               [0,0,1,1,1,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0]]
+    
     print("User updated positions : ")
-    generateGridStart(cellGrid=cellGrid, startGrid=startGrid)
+    generateGridStart(cellGrid=cellGrid, startGrid=startGrid2)
     printGrid(cellGrid)
+
+    # Preparing the grid and giving info about number of neighbours
+    updateGridNeighbours(cellGrid=cellGrid)
+    print("The grid is now prepared and we are ready to start the Game of life !")
 
 
     n = int(input("Enter number of cycles : "))
 
     for i in range(n):
-        print("Cycle Number = ", i)
+        time.sleep(1)
+        print("Cycle Number = ", i+1)
         cellGrid = singleCycle(cellGrid=cellGrid)
         printGrid(cellGrid=cellGrid)
+        
     
 
 def generateGrid(xDim, yDim, choice):
@@ -57,25 +78,39 @@ def printGrid(cellGrid):
             cell.printCell()
         print()
 
+# Updates the grid with the starting connfiguration provided by the user
 def generateGridStart(cellGrid, startGrid):
     for row in range(len(cellGrid)):
         for col in range(len(cellGrid[row])):
             cellGrid[row][col].isAlive = startGrid[row][col]
 
+# Updates the grid, killing off the cells that must die
 def updateGrid(cellGrid):
-    pass
+    for row in range(1, len(cellGrid)-1):
+        for col in range(1, len(cellGrid[row])-1):
+            cellGrid[row][col].updateAlive()
 
+#Updates everyone's neighbours, based on the new grid
 def updateGridNeighbours(cellGrid):
-    pass
+    for row in range(1, len(cellGrid)-1):
+        for col in range(1, len(cellGrid[row])-1):
+            #top = row--, bottom = row++, left = col--, right = col++
+            topRight = cellGrid[row - 1][col + 1]
+            topLeft = cellGrid[row - 1][col - 1]
+            bottomRight = cellGrid[row + 1][col + 1]
+            bottomLeft = cellGrid[row + 1][col - 1]
+            top = cellGrid[row - 1][col]
+            bottom = cellGrid[row + 1][col]
+            left = cellGrid[row][col - 1]
+            right = cellGrid[row][col + 1]
+
+            cellGrid[row][col].updateNeighbours(topRightNeighbour=topRight, topLeftNeighbour=topLeft, bottomRightNeighbour=bottomRight, bottomLeftNeighbour=bottomLeft, topNeighbour=top, bottomNeighbour=bottom, rightNeighbour=right, leftNeighbour=left)
 
 def singleCycle(cellGrid):
     updateGrid(cellGrid=cellGrid)
     updateGridNeighbours(cellGrid=cellGrid)
 
     return cellGrid
-
-
-    
 
 if __name__ == "__main__":
     main()
